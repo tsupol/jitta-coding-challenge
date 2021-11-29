c9f79e8ddc126675ad59633c946a04df57a25e6a
Ton Supol <ton.supol@gmail.com>
2021-11-29 18:06:46 +0700

    separate the q1 codes

--- jitta-coding-challenge-q1-faster.js
+++ jitta-coding-challenge-q1-faster.js
@@ -1,39 +1,5 @@
 /**
  * Find the best buy and sell date.
- * Method: bruteforce
- * O(n^2)
- *
- * @param prices
- */
-function q1MaxProfitBruteForce (prices) {
-
-	if (prices.length < 2) {
-		throw new Error('Cannot compare when the price data is less than 2 items.');
-	}
-
-	let maxProfit = Number.NEGATIVE_INFINITY;
-	let buyIndex = 0;
-	let sellIndex = 1;
-
-	for (let i = 0; i < prices.length; i++) {
-		const buyPrice = prices[i].closePrice;
-		for (let j = i + 1; j < prices.length; j++) {
-			if (prices[j].closePrice - buyPrice > maxProfit) {
-				maxProfit = prices[j].closePrice - buyPrice;
-				buyIndex = i;
-				sellIndex = j;
-			}
-		}
-	}
-
-	console.log("----- question 1 BruteForce -----");
-	console.log(`buy: date: ${prices[buyIndex].date}, price: ${prices[buyIndex].closePrice}`);
-	console.log(`sell: date: ${prices[sellIndex].date}, price: ${prices[sellIndex].closePrice}`);
-	console.log(`profit: ${prices[sellIndex].closePrice - prices[buyIndex].closePrice}`);
-}
-
-/**
- * Find the best buy and sell date.
  * Method:
  * 1 - Find the max.
  * 2 - Find the min on the left.
